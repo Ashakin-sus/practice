73049600bdbc7b3f3a81a50fab1a1ef9b1d3cf7d
Ashakin-sus <kazihabibul2@gmail.com>
2022-02-03 10:37:10 +0600

    hudai

---
+++ Restart.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 12 21:31:48 2021
+
+@author: Ashakin
+"""
+
+import os
+os.system("restart /r /t 1")
---
+++ Shutdown.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  5 21:20:08 2021
+
+@author: Ashakin
+"""
+
+import os
+os.system("shutdown /s /t 1")
